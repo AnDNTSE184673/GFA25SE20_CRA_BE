e4c0294f6cef8f1f5f398e46406104c590db5607
AnDNTSE184673 <andntse184673@fpt.edu.vn>
2025-11-02 03:49:17 +0700

    Simple Register and Login, Enums need adjust for Gender and Role

---
+++ Repository/DTO/RequestDTO/RegisterRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DTO.RequestDTO
+{
+    public class RegisterRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Fullname { get; set; }
+        public string? Address { get; set; }
+        public int Gender { get; set; }
+    }
+}
---
+++ Repository/Repositories/UserRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Base;
+using Repository.Data;
+using Repository.Data.Entities;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Repositories
+{
+    public class UserRepository : GenericRepository<User>, IUserRepository
+    {
+        private readonly CRA_DbContext _context;
+        public UserRepository(CRA_DbContext context) : base(context)
+        {
+            _context = context;
+        }
+        public Task<User?> Authentication(string email, string password)
+        {
+            var user = _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
+            return user;
+        }
+
+        public User? GetByEmail(string email)
+        {
+            var user = _context.Users
+                .Include(u => u.Role)
+                .FirstOrDefault(u => u.Email == email);
+            return user;
+        }
+    }
+}
