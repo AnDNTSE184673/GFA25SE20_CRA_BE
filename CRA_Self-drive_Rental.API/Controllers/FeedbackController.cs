7ad2fd3f63acab5340ac39d88ccc8de5fb03cd31
TrNgQuangVinh <vinhtnqse184691@fpt.edu.vn>
2025-11-14 11:34:27 +0700

    [WIP] Feedback

---
+++ CRA_Self-drive_Rental.API/Controllers/FeedbackController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRA_Self_drive_Rental.API.Controllers
+{
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IFeedbackService _feedbackServ;
+
+        public FeedbackController(IFeedbackService feedbackServ)
+        {
+            _feedbackServ = feedbackServ;
+        }
+
+        public async Task<IActionResult> CreateFeedback(FeedbackForm form)
+        {
+
+        }
+    }
+}
