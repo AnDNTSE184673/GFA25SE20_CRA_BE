30fcd1f2e813946393f0cc4325b0744578f70828
AnDNTSE184673 <andntse184673@fpt.edu.vn>
2025-12-09 23:28:04 +0700

    Create Extension For Booking With Booking/ExtendBooking and update a Payment manual with UpdatePayment/Booking/Payment

---
+++ Repository/DTO/RequestDTO/BookingExtensionRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DTO.RequestDTO
+{
+    public class BookingExtensionRequest
+    {
+        [Required]
+        public Guid BookingId { get; set; }
+        [Required]
+        public Guid CarId { get; set; }
+        [Required]
+        [Range(1, 30, ErrorMessage = "Extension time must be between 1 and 30 days.")]
+        public int TimeExtInDays { get; set; }
+    }
+}
---
+++ Repository/DTO/RequestDTO/Payment/UpdatePaymentRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.DTO.RequestDTO.Payment
+{
+    public class UpdatePaymentRequest
+    {
+        [Required]
+        public Guid PaymentId { get; set; }
+        public string Status { get; set; }
+    }
+}
