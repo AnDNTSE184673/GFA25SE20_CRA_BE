44b861978118a3b802b9ac5393283d05a1a68a24
AnDNTSE184673 <andntse184673@fpt.edu.vn>
2026-01-15 10:52:25 +0700

    CarTravelLog and TollBooth

---
+++ Repository/Data/Entities/CarTravelLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data.Entities
+{
+    public class CarTravelLog
+    {
+        public int Id { get; set; }
+        public Guid CarId { get; set; }
+        public Guid BookingId { get; set; }
+        public DateTime TravelDate { get; set; }
+        public int TollBoothId { get; set; }
+        public decimal ChargeAmount { get; set; }
+        [ForeignKey("CarId")]
+        public virtual Car Car { get; set; }
+        [ForeignKey("BookingId")]
+        public virtual Booking Booking { get; set; }
+        [ForeignKey("TollBoothId")]
+        public virtual TollBooth TollBooth { get; set; }
+    }
+}
--- Repository/Migrations/20260115034130_TollBoothAndCarTravelLog.Designer.cs
+++ Repository/Migrations/20260115034130_TollBoothAndCarTravelLog.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Repository.Data;
@@ -11,9 +12,11 @@ using Repository.Data;
 namespace Repository.Migrations
 {
     [DbContext(typeof(CRA_DbContext))]
-    partial class CRA_DbContextModelSnapshot : ModelSnapshot
+    [Migration("20260115034130_TollBoothAndCarTravelLog")]
+    partial class TollBoothAndCarTravelLog
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
