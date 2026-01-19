c4130c3cf9696246264f1c036dbcf1cf1a75bbb7
AnDNTSE184673 <andntse184673@fpt.edu.vn>
2026-01-19 22:33:51 +0700

    TollBoothController with GetAll, Comment CreateRandomCarTravelLog in CheckIn in Schedule and create Noti to CarOwner and Cus when create CarTravelLog

---
+++ Service/Services/Implementation/TollBoothService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Repository.Base;
+using Repository.DTO.ResponseDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services.Implementation
+{
+    public class TollBoothService : ITollBoothService
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        public TollBoothService(UnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<List<TollBoothView>> GetAllTollBoothsAsync()
+        {
+            var tollBooths = await _unitOfWork._tollRepo.GetAllAsync();
+            return _mapper.Map<List<TollBoothView>>(tollBooths);
+        }
+    }
+}
